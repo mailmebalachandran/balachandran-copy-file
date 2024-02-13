fce88abd9eb73460c403756b629edcc6efe8665e
dol_2 <dol_2@devopslabs.tech>
2024-02-13 20:27:20 +0800

    sha was fixed

--- balachandran-copy-file.rb
+++ balachandran-copy-file.rb
@@ -2,7 +2,7 @@ class BalachandranCopyFile < Formula
     desc "Copy from one file from one place to another place"
     homepage "https://github.com/mailmebalachandran/homebrew-balachandran-copy-file"
     url "https://github.com/mailmebalachandran/homebrew-balachandran-copy-file/archive/v1.0.0.tar.gz"
-    sha256 "TSample Test for Balachandran"
+    sha256 "bd87d639c3937cc5355a600c20e8195a7e52772d334ff8cdd47bbe7f9345f436"
   
     depends_on "rust" => :build
   
